19179dd4e9a686f2be78357f45df59d0e935571a
larrygf02 <raulgf_02@hotmail.com>
2019-05-23 22:16:34 -0500

    Desconectar usuarios

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -1,14 +1,25 @@
 var socket = io();
+var params = new URLSearchParams( window.location.search );
+
+if (!params.has('nombre')) {
+    window.location = 'index.html';
+    throw new Error('El nombre es necesario');
+}
+
+var usuario = {
+    nombre: params.get('nombre')
+}
 
 socket.on('connect', function() {
     console.log('Conectado al servidor');
+    socket.emit('entrarChat', usuario, function( resp) {
+        console.log(resp);
+    })
 });
 
 // escuchar
 socket.on('disconnect', function() {
-
     console.log('Perdimos conexión con el servidor');
-
 });
 
 
